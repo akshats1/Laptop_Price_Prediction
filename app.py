5e46151e0b8d0347cdcce0df5ecbf069cd53103f
akshats1 <akshats1@users.noreply.github.com>
2024-02-11 23:02:45 +0530

    Update app.py
    read_pickle()

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import streamlit as st
 import pickle
 import numpy as np
+import pandas as pd
+
+#df = pd.read_pickle("file.pkl")
 
 # import the model
 pipe = pickle.load(open('pipe.pkl','rb'))
-df = pickle.load(open('df.pkl','rb'))
+#df = pickle.load(open('df.pkl','rb'))
+df=pd.read_pickle('df.pkl')
 
 st.title("Laptop Predictor")
 
